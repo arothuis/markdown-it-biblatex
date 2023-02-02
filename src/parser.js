6c58017f4aed8e4ee9a3a94cf259225911c38ae4
Pridon Tetradze <pridontetradze@gmail.com>
2023-02-02 17:50:51 +0400

    chore: initial linting, formatting and their options.

--- src/parser.js
+++ src/parser.js
@@ -1,122 +1,115 @@
 function parser(context) {
-    const { 
-        suppressAuthorMark, 
-        authorOnlyMark, 
-        compositeMark,
-        infixMark,
-        bibliographyMark,
-    } = context.options;
+  const { suppressAuthorMark, authorOnlyMark, compositeMark, infixMark, bibliographyMark } =
+    context.options;
 
-    const marks = [suppressAuthorMark, authorOnlyMark, compositeMark]
-        .map(mark => mark.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
-        .join("|");
-    const validCitation = new RegExp(`(?<=^\\\[)((${marks})?@)(.+?)[^\](?=\])`);
+  const marks = [suppressAuthorMark, authorOnlyMark, compositeMark]
+    .map((mark) => mark.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
+    .join('|');
+  const validCitation = new RegExp(`(?<=^\\\[)((${marks})?@)(.+?)[^\](?=\])`);
 
-    function reference(state, silent) {
-        const { pos, posMax, src } = state;
-        
-        if (state.env.bib === undefined) { 
-            state.env.bib = { 
-                refs: [],
-            };
-        }
+  function reference(state, silent) {
+    const { pos, posMax, src } = state;
 
-        if (pos + 4 > posMax) { 
-            return false; 
-        }
-
-        const match = src.slice(pos).match(validCitation);
-        
-        if (match === null) {
-            return false;
-        }
-    
-        if (match[0].slice(0, suppressAuthorMark.length) === suppressAuthorMark) {
-            return processReference(state, match[0], "suppress-author");            
-        }
-    
-        if (match[0].slice(0, authorOnlyMark.length) === authorOnlyMark) {
-            return processReference(state, match[0], "author-only");
-        }
-    
-        if (match[0].slice(0, compositeMark.length) === compositeMark) {
-            return processReference(state, match[0], "composite");
-        }
-
-        return processReference(state, match[0]);
-    }
-    
-    function processReference(state, ref, mode) {
-        let infix = null;
-
-        const citationItems = ref
-            .split(";")
-            .map(rawItem => {
-                const item = rawItem.trim();
-                
-                const label = item.split("{")[0].split("#")[0].split("@")[1];
-                const findLocator = item.match(/(?<=#)(.+?)(?=({|$))/) || [];
-                const findAffixes = item.match(/(?<={)(.+?)(?=})/g) || [];
-    
-                let prefix = findAffixes[0];
-                if (mode === "composite") {
-                    if (infix === null) {
-                        foundInfix = findAffixes.filter(i => i[0] === infixMark)[0];
-                        infix = (foundInfix || "").slice(1);
-                    }
-                    
-                    prefix = findAffixes.filter(i => i[0] !== infixMark[0])[0];
-                }
-
-                return {
-                    label, 
-                    id: context.bibData.ids[label], 
-                    number: state.env.bib.refs.length, 
-                    prefix, 
-                    locator: findLocator[0],
-                };
-            });
-    
-        const token = state.push("biblatex_reference", "", 0);
-        token.meta = {
-            citation: {
-                citationItems,
-                properties: {
-                    noteIndex: 0,
-                    mode,
-                    infix,
-                },
-            }
-        };
-    
-        state.env.bib.refs.push(token.meta);
-        state.pos += ref.length + 2;
-    
-        return true;
-    }
-    
-    function bibliography(state, startLine, endLine, silent) {
-        const start = state.bMarks[startLine] + state.tShift[startLine];
-    
-        if (state.src.slice(start, start + bibliographyMark.length) !== bibliographyMark) {
-            return false;
-        }
-    
-        state.push("biblatex_bibliography_open", "", 0);
-        state.push("biblatex_bibliography_contents", "", 1);
-        state.push("biblatex_bibliography_close", "", 0);
-    
-        state.line++;
-    
-        return true;
+    if (state.env.bib === undefined) {
+      state.env.bib = {
+        refs: [],
+      };
     }
 
-    return {
-        reference,
-        bibliography,
+    if (pos + 4 > posMax) {
+      return false;
+    }
+
+    const match = src.slice(pos).match(validCitation);
+
+    if (match === null) {
+      return false;
+    }
+
+    if (match[0].slice(0, suppressAuthorMark.length) === suppressAuthorMark) {
+      return processReference(state, match[0], 'suppress-author');
+    }
+
+    if (match[0].slice(0, authorOnlyMark.length) === authorOnlyMark) {
+      return processReference(state, match[0], 'author-only');
+    }
+
+    if (match[0].slice(0, compositeMark.length) === compositeMark) {
+      return processReference(state, match[0], 'composite');
+    }
+
+    return processReference(state, match[0]);
+  }
+
+  function processReference(state, ref, mode) {
+    let infix = null;
+
+    const citationItems = ref.split(';').map((rawItem) => {
+      const item = rawItem.trim();
+
+      const label = item.split('{')[0].split('#')[0].split('@')[1];
+      const findLocator = item.match(/(?<=#)(.+?)(?=({|$))/) || [];
+      const findAffixes = item.match(/(?<={)(.+?)(?=})/g) || [];
+
+      let prefix = findAffixes[0];
+      if (mode === 'composite') {
+        if (infix === null) {
+          foundInfix = findAffixes.filter((i) => i[0] === infixMark)[0];
+          infix = (foundInfix || '').slice(1);
+        }
+
+        prefix = findAffixes.filter((i) => i[0] !== infixMark[0])[0];
+      }
+
+      return {
+        label,
+        id: context.bibData.ids[label],
+        number: state.env.bib.refs.length,
+        prefix,
+        locator: findLocator[0],
+      };
+    });
+
+    const token = state.push('biblatex_reference', '', 0);
+    token.meta = {
+      citation: {
+        citationItems,
+        properties: {
+          noteIndex: 0,
+          mode,
+          infix,
+        },
+      },
     };
+
+    state.env.bib.refs.push(token.meta);
+    state.pos += ref.length + 2;
+
+    return true;
+  }
+
+  function bibliography(state, startLine, endLine, silent) {
+    const start = state.bMarks[startLine] + state.tShift[startLine];
+
+    if (state.src.slice(start, start + bibliographyMark.length) !== bibliographyMark) {
+      return false;
+    }
+
+    state.push('biblatex_bibliography_open', '', 0);
+    state.push('biblatex_bibliography_contents', '', 1);
+    state.push('biblatex_bibliography_close', '', 0);
+
+    state.line++;
+
+    return true;
+  }
+
+  return {
+    reference,
+    bibliography,
+  };
 }
 
 module.exports = {
-    parser,
+  parser,
 };
--- src/renderer.js
+++ src/renderer.js
@@ -1,88 +1,90 @@
 function renderer(context) {
-    const { citeproc } = context;
-    const { 
-        wrapBibliography, 
-        bibliographyTitle,
-        bibliographyContentsWrapper,
-        bibliographyEntryWrapper,
-    } = context.options;
+  const { citeproc } = context;
+  const {
+    wrapBibliography,
+    bibliographyTitle,
+    bibliographyContentsWrapper,
+    bibliographyEntryWrapper,
+  } = context.options;
 
-    function reference(tokens, idx, options, env, slf) {
-        if (env.bib.currentRefs === undefined) {
-          env.bib.currentRefs = [];
-        }
-
-        env.bib.currentRefs.push(tokens[idx].meta);
-
-        const citation = citeproc.processCitationCluster(tokens[idx].meta.citation, [], []);
-        return citation[1][0][1];
+  function reference(tokens, idx, options, env, slf) {
+    if (env.bib.currentRefs === undefined) {
+      env.bib.currentRefs = [];
     }
 
-    function bibliographyOpen(tokens, idx, options, env, slf) {
-        if (env.bib === undefined || env.bib.currentRefs === undefined) {
-            return "";
-        }
+    env.bib.currentRefs.push(tokens[idx].meta);
 
-        let rendered = "";
-        if (wrapBibliography === true) {
-            rendered += '<div class="bibliography">\n';
-        }
+    const citation = citeproc.processCitationCluster(tokens[idx].meta.citation, [], []);
+    return citation[1][0][1];
+  }
 
-        rendered += bibliographyTitle + "\n";
-
-        return rendered;
+  function bibliographyOpen(tokens, idx, options, env, slf) {
+    if (env.bib === undefined || env.bib.currentRefs === undefined) {
+      return '';
     }
 
-    function bibliographyContents(tokens, idx, options, env, slf) {
-        if (env.bib === undefined || env.bib.currentRefs === undefined) {
-            return "";
-        }
-
-        const seen = [];
-        env.bib.currentRefs.forEach(ref => {
-            ref.citation.citationItems.forEach(item => {
-                seen.push(item.id);
-            });
-        });
-    
-        citeproc.updateItems(seen);
-        const [_, contents] = citeproc.makeBibliography();
-
-        if (bibliographyEntryWrapper !== "div") {
-            contents.forEach((content, idx) => {
-                contents[idx] = content
-                                .replace("<div", "<" + bibliographyEntryWrapper)
-                                .replace("</div", "</" + bibliographyEntryWrapper)
-            })
-        }
-
-        return `<${bibliographyContentsWrapper} class="bibliography-contents">\n` 
-            + contents.join("")
-            + `</${bibliographyContentsWrapper}>\n`;
+    let rendered = '';
+    if (wrapBibliography === true) {
+      rendered += '<div class="bibliography">\n';
     }
 
-    function bibliographyClose(tokens, idx, options, env, slf) {
-        if (env.bib === undefined || env.bib.currentRefs === undefined) {
-            return "";
-        }
+    rendered += bibliographyTitle + '\n';
 
-        env.bib.currentRefs = [];
+    return rendered;
+  }
 
-        if (wrapBibliography) {
-            return "</div>\n";
-        }
-
-        return "";
+  function bibliographyContents(tokens, idx, options, env, slf) {
+    if (env.bib === undefined || env.bib.currentRefs === undefined) {
+      return '';
     }
 
-    return {
-        reference,
-        bibliographyOpen,
-        bibliographyContents,
-        bibliographyClose,
-    };
+    const seen = [];
+    env.bib.currentRefs.forEach((ref) => {
+      ref.citation.citationItems.forEach((item) => {
+        seen.push(item.id);
+      });
+    });
+
+    citeproc.updateItems(seen);
+    const [_, contents] = citeproc.makeBibliography();
+
+    if (bibliographyEntryWrapper !== 'div') {
+      contents.forEach((content, idx) => {
+        contents[idx] = content
+          .replace('<div', '<' + bibliographyEntryWrapper)
+          .replace('</div', '</' + bibliographyEntryWrapper);
+      });
+    }
+
+    return (
+      `<${bibliographyContentsWrapper} class="bibliography-contents">\n` +
+      contents.join('') +
+      `</${bibliographyContentsWrapper}>\n`
+    );
+  }
+
+  function bibliographyClose(tokens, idx, options, env, slf) {
+    if (env.bib === undefined || env.bib.currentRefs === undefined) {
+      return '';
+    }
+
+    env.bib.currentRefs = [];
+
+    if (wrapBibliography) {
+      return '</div>\n';
+    }
+
+    return '';
+  }
+
+  return {
+    reference,
+    bibliographyOpen,
+    bibliographyContents,
+    bibliographyClose,
+  };
 }
 
 module.exports = {
-    renderer,
+  renderer,
 };
