2286eaa9f72b1509c100c2db546ea776f7b751ed
Alex Rothuis <alex.rothuis@gmail.com>
2023-03-03 16:12:58 +0100

    chore: remove redundant test

--- test/integration.test.js
+++ test/integration.test.js
@@ -24,7 +24,6 @@ describe('markdown-it plug-in', () => {
       ['no cited items, no bibliography, despite mark', 'no-items'],
       ['infix for composite citations', 'infix-composite'],
       ['each bibliography contains only references directly before it', 'multiple-bibliographies'],
-      ['if there are no references, do not make a bibliography', 'no-refs-no-bibliography'],
     ];
 
     examples.forEach(([specification, fixtureName]) => {
